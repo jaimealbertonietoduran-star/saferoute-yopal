b02166d5cbd16b02d4d43f24c3bd080dd67497b7
Jaime Nieto <jaimealbertonietoduran@gmail.com>
2026-07-05 18:49:57 -0500

    fix: agregar ignorar logs de notificaciones de Android

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import { registerRootComponent } from 'expo';
+import { LogBox } from 'react-native';
 import React from 'react';
 import App from './App';
 import ErrorBoundary from './components/ErrorBoundary';
+
+LogBox.ignoreLogs(['expo-notifications: Android Push notifications']);
 
 function Root() {
   return (
